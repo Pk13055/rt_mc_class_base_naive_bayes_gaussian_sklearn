f8539caebf37b1a0e5de30a3bccb8d12531d4d09
Pk13055 <Pk13055@users.noreply.github.com>
2022-11-11 21:50:42 +0400

    [Addition] Naive bayes gaussian model

--- app/algorithm/model/mc_classifier.py
+++ app/algorithm/model/mc_classifier.py
@@ -1,85 +1,65 @@
+import os
+import warnings
 
-import numpy as np, pandas as pd
 import joblib
-import sys
-import os, warnings
-warnings.filterwarnings('ignore') 
+from sklearn.naive_bayes import GaussianNB
 
 
-
-from sklearn.linear_model import LogisticRegression
-
-
+warnings.filterwarnings("ignore")
 model_fname = "model.save"
+MODEL_NAME = "multi_class_base_naive_bayes_gaussian_sklearn"
 
-MODEL_NAME = "multi_class_base_logistic_regression_sklearn"
 
+class Classifier:
+    def __init__(self, var_smoothing: float = 1e-09, **kwargs) -> None:
+        self.var_smoothing = var_smoothing
+        self.model = self.build_model()
 
-class Classifier(): 
-    
-    def __init__(self, penalty="elasticnet", C = 1.0, l1_ratio=0.5, **kwargs) -> None:
-        self.penalty = penalty
-        self.C = np.float(C)
-        if self.penalty == 'elasticnet':
-            self.l1_ratio = l1_ratio if l1_ratio is not None and 0 <= l1_ratio <= 1 else 0.5
-        else: 
-            self.l1_ratio = None
-        self.model = self.build_model()     
-        
-        
-    def build_model(self): 
-        model = LogisticRegression(penalty = self.penalty, C = self.C, solver='saga', l1_ratio=self.l1_ratio)
+    def build_model(self):
+        model = GaussianNB(var_smoothing=self.var_smoothing)
         return model
-    
-    
-    def fit(self, train_X, train_y):        
-        self.model.fit(train_X, train_y)            
-        
-    
-    def predict(self, X, verbose=False): 
+
+    def fit(self, train_X, train_y):
+        self.model.fit(train_X, train_y)
+
+    def predict(self, X, verbose=False):
         preds = self.model.predict(X)
-        return preds          
-        
-    
-    def predict_proba(self, X, verbose=False): 
+        return preds
+
+    def predict_proba(self, X, verbose=False):
         preds = self.model.predict_proba(X)
-        return preds 
-    
+        return preds
 
     def summary(self):
         self.model.get_params()
-        
-    
-    def evaluate(self, x_test, y_test): 
+
+    def evaluate(self, x_test, y_test):
         """Evaluate the model and return the loss and metrics"""
         if self.model is not None:
-            return self.model.score(x_test, y_test)        
+            return self.model.score(x_test, y_test)
 
-    
-    def save(self, model_path): 
+    def save(self, model_path):
         joblib.dump(self, os.path.join(model_path, model_fname))
-        
-
 
     @classmethod
-    def load(cls, model_path):         
+    def load(cls, model_path):
         logisticregression = joblib.load(os.path.join(model_path, model_fname))
-        # print("where the load function is getting the model from: "+ os.path.join(model_path, model_fname))        
+        # print("where the load function is getting the model from: "+ os.path.join(model_path, model_fname))
         return logisticregression
 
 
 def save_model(model, model_path):
     # print(os.path.join(model_path, model_fname))
-    joblib.dump(model, os.path.join(model_path, model_fname)) #this one works
+    joblib.dump(model, os.path.join(model_path, model_fname))  # this one works
     # print("where the save_model function is saving the model to: " + os.path.join(model_path, model_fname))
-    
 
-def load_model(model_path): 
-    try: 
-        model = joblib.load(os.path.join(model_path, model_fname))   
-    except: 
-        raise Exception(f'''Error loading the trained {MODEL_NAME} model. 
-            Do you have the right trained model in path: {model_path}?''')
+
+def load_model(model_path):
+    try:
+        model = joblib.load(os.path.join(model_path, model_fname))
+    except:
+        raise Exception(
+            f"""Error loading the trained {MODEL_NAME} model.
+            Do you have the right trained model in path: {model_path}?"""
+        )
     return model
-
-
